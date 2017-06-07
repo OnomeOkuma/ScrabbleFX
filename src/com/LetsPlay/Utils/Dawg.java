64024a9fb1cbcb40cb23171639464b60eb27573d
onome_okuma <okumaonome@gmail.com>
2017-06-07 21:30:16 +0100

    Added Test case for TileBag and Started work on the Dawg Data Structure

---
+++ src/com/LetsPlay/Utils/Dawg.java
@@ -0,0 +1,16 @@
+package com.LetsPlay.Utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.File;
+import java.io.FileReader;
+
+public class Dawg {
+		private HashMap<String, ArrayList> wordsearch = new HashMap<String, ArrayList>(200000);
+		File wordlocation = new File("/home/onome/Wordlist");
+		
+		public Dawg(){
+		}
+	
+		
+}
